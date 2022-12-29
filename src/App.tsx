9987cde2fe19048b202f0067e451e672c5d79bbb
JinHo Shin <jinho@draph.ai>
2022-12-29 13:13:18 +0900

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("")
+  const onChange = ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
+    console.log(currentTarget.value);
+    setValue(currentTarget.value);
+  }
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  }
+  
   return (
    <div>
-    <Circle bgColor="tomato" borderColor="yellow" />
-    <Circle bgColor="teal" text="Excellent"/>
+    <form onSubmit={onSubmit}>
+      <input value={value} type="text" placeholder="username" onChange={onChange}/>
+      <button>Log in</button>
+    </form>
    </div>
   );
 }
